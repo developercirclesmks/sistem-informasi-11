28ac579dc60890858af376d5940a99b08b209a87
tasyanabilah <mayormain4@gmail.com>
2023-10-12 15:38:21 +0800

    feat: AboutUsCard (update)

--- src/components/molecules/aboutuscard/AboutUsCard.tsx
+++ src/components/molecules/aboutuscard/AboutUsCard.tsx
@@ -6,25 +6,30 @@ import {
   IonCardTitle,
 } from "@ionic/react";
 import React from "react";
+import style from "./AboutUsCard.module.css"
 
 const AboutUsCard: React.FC = () => {
-  return (
+return (
     <main>
-      <IonCard>
-        <img
-          alt="Silhouette of mountains"
-          src="https://ionicframework.com/docs/img/demos/card-media.png"
-        />
-        <IonCardHeader>
-          <IonCardTitle>Tasya Nabila Hasanuddin</IonCardTitle>
-          <IonCardSubtitle>3rd Year Undergraduate Student at Unhas</IonCardSubtitle>
-        </IonCardHeader>
+      <IonCard className={style.ionCard}>
+        <section>
+          <img className={style.img}
+            alt="Silhouette of mountains"
+            src="https://ionicframework.com/docs/img/demos/card-media.png"
+            />
+        </section>
+        
+        <section className={style.contentCtn}>
+          <IonCardHeader className={style.sectionHeader}>
+            <IonCardTitle className={style.name}>Tasya Nabila Hasanuddin</IonCardTitle>
+            <IonCardSubtitle className={style.subtitle}>3rd Year Undergraduate Student at Unhas</IonCardSubtitle>
+          </IonCardHeader>
 
-        <IonCardContent>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati doloribus quisquam assumenda iusto laboriosam. 
-          Cum aperiam hic a laboriosam laudantium esse cupiditate! Sit impedit deserunt facere sapiente reprehenderit eveniet 
-          voluptatem.
-        </IonCardContent>
+          <IonCardContent className={style.sectionContent}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, voluptatem esse!
+          </IonCardContent>
+        </section>
+
       </IonCard>
     </main>
   );
