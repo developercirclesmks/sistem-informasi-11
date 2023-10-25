7746e44ad7fc240053ec7f9576f24bf48d576ea8
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-10-25 16:36:12 +0800

    feat: Dashboard - Responsive Layout & Dynamic Data

---
+++ src/components/organisms/Dashboard/ExamList/ExamListsCard.tsx
@@ -0,0 +1,83 @@
+import {
+	IonButton,
+	IonCard,
+	IonCardContent,
+	IonContent,
+	IonHeader,
+	IonIcon,
+	IonInfiniteScroll,
+	IonItem,
+	IonItemGroup,
+	IonLabel,
+	IonPage,
+	IonText,
+	IonTitle,
+	IonToolbar,
+} from "@ionic/react";
+import React from "react";
+import style from "./ExamListsCard.module.css";
+import { Exams } from "../../../dummydata";
+
+interface ExamListCardInterface {
+	passed?: boolean;
+}
+
+const ExamListsCard: React.FC<ExamListCardInterface> = (props) => {
+	const { passed = false } = props;
+
+	const currentDateTime = new Date();
+
+	const PassedList = Exams.filter(
+		(exam) => exam.schedule.getTime() <= currentDateTime.getTime()
+	).sort((a, b) => a.schedule.getTime() - b.schedule.getTime());
+
+	const UpcomingList = Exams.filter(
+		(exam) => exam.schedule.getTime() > currentDateTime.getTime()
+	).sort((a, b) => a.schedule.getTime() - b.schedule.getTime());
+
+	const ExamList = passed ? PassedList : UpcomingList;
+	const examLength = ExamList.length
+
+
+	return (
+		<>
+			<IonCard className="ion-padding" color={"light"}>
+				<IonItem lines="none" color={"light"}>
+					{passed ? "Exams Done" : "Upcoming Exams"} :
+				</IonItem>
+				<IonCardContent>
+					<IonItemGroup className={style.Group}>
+						{ExamList.map((exam, index) => {
+							const scheduleDate = exam.schedule;
+							const day = scheduleDate.getDate();
+							const month = scheduleDate.getMonth() + 1;
+							const year = scheduleDate.getFullYear();
+							const hour = scheduleDate.getHours();
+							const minute = scheduleDate.getMinutes();
+
+							return (
+								<IonItem button={true} detail={false}>
+									<IonLabel>
+										<strong>{exam.label}</strong>
+										<div className={style.ItemContent}>
+											<IonText>By : {exam.creator}</IonText>
+											<IonText className={style.date}>
+												{`${day < 10 ? "0" + day : day}/${month}/${year}`} -{" "}
+												{`${hour < 10 ? "0" + hour : hour}:${
+													minute < 10 ? "0" + minute : minute
+												}`}
+											</IonText>
+										</div>
+										<br />
+									</IonLabel>
+								</IonItem>
+							);
+						})}
+					</IonItemGroup>
+				</IonCardContent>
+			</IonCard>
+		</>
+	);
+};
+
+export default ExamListsCard;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,47 +1,59 @@
 import {
-  IonMenu,
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonSearchbar,
-  IonTitle,
-  IonToolbar,
-  IonButtons,
-  IonMenuButton,
+	IonMenu,
+	IonContent,
+	IonHeader,
+	IonPage,
+	ItemReorderEventDetail,
 } from "@ionic/react";
 import React from "react";
 import style from "./Dashboard.module.css";
+import Sidebar from "../components/organisms/Sidebar/Sidebar";
+import Navbar from "../components/organisms/Navbar/Navbar";
+import ProfileCard from "../components/organisms/Dashboard/ProfileCard/ProfileCard";
+import StatsCard from "../components/organisms/Dashboard/StatsCard/StatsCard";
+import { Link } from "react-router-dom";
+import AboutUsCard from "../components/molecules/aboutuscard/AboutUsCard";
+import ExamListCard from "../components/organisms/Dashboard/ExamList/ExamListsCard";
+import { passedLength, upcomingLength} from "../components/dummydata";
+
 const Dashboard: React.FC = () => {
-  return (
-    <>
-    <IonMenu contentId="main-content" type="push">
-      <IonHeader>
-        <IonToolbar>
-          <IonToolbar>
-              </IonToolbar>
-          <IonTitle>Menu Content</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">This is the menu content.</IonContent>
-    </IonMenu>
-    <IonPage id="main-content">
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton></IonMenuButton>
-          </IonButtons>
-        <section className={style.toolbar}>
-          <IonTitle>Dashboard</IonTitle>
-            <IonSearchbar></IonSearchbar>
-              <section>Profile</section>
-          </section>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-      Dashboard Content  </IonContent>
-    </IonPage>
-   </>
-  );
+	return (
+		<>
+			<Sidebar />
+			<IonPage id="main-content">
+				<Navbar />
+				<IonContent color="primary">
+					<div>
+						<ProfileCard />
+					</div>
+					<div className={style.cardsRow3}>
+						{statlist.map((maps, index) => (
+							<StatsCard key={index} label={maps.label} value={maps.value} />
+						))}
+					</div>
+					<div className={style.cardsRow2}>
+						<ExamListCard passed/>
+						<ExamListCard />
+					</div>
+				</IonContent>
+			</IonPage>
+		</>
+	);
 };
 
 export default Dashboard;
+
+const statlist = [
+	{
+		label: "Exams Done",
+		value: passedLength,
+	},
+	{
+		label: "Time Spent Today",
+		value: "3hr",
+	},
+	{
+		label: "Upcoming Exams",
+		value: upcomingLength,
+	},
+];
