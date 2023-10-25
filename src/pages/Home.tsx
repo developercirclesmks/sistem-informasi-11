a64f63c36709238759548c5461d155083051d5c5
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-10-25 16:33:25 +0800

    test : Sidebar - Testing sidebar in homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,31 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
-import Navbar from '../components/organisms/Navbar/Navbar';
-import Hero from '../components/organisms/Hero/Hero';
+import {
+	IonContent,
+	IonHeader,
+	IonMenu,
+	IonPage,
+	IonTitle,
+	IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
+import Navbar from "../components/organisms/Navbar/Navbar";
+import Hero from "../components/organisms/Hero/Hero";
+import Sidebar from "../components/organisms/Sidebar/Sidebar";
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-				<Navbar/>
-      </IonHeader>
-      <IonContent fullscreen>
-				<Hero/>
-      </IonContent>
-    </IonPage>
-  );
+	return (
+		<>
+			<Sidebar />
+			<IonPage id="main-content">
+				<IonHeader>
+					<Navbar />
+				</IonHeader>
+				<IonContent fullscreen>
+					<Hero />
+				</IonContent>
+			</IonPage>
+		</>
+	);
 };
 
 export default Home;
