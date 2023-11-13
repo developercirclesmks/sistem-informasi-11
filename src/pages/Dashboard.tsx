3cc29fc90a409fccbdebcbda78120bec2598d1c5
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-11-13 13:46:32 +0800

    feat: Create Segment - create exam

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,6 +4,7 @@ import {
 	IonHeader,
 	IonPage,
 	ItemReorderEventDetail,
+	IonCard,
 } from "@ionic/react";
 import React from "react";
 import style from "./Dashboard.module.css";
@@ -14,7 +15,8 @@ import StatsCard from "../components/organisms/Dashboard/StatsCard/StatsCard";
 import { Link } from "react-router-dom";
 import AboutUsCard from "../components/molecules/aboutuscard/AboutUsCard";
 import ExamListCard from "../components/organisms/Dashboard/ExamList/ExamListsCard";
-import { passedLength, upcomingLength} from "../components/dummydata";
+import { passedLength, upcomingLength } from "../components/dummydata";
+import DashboardHero from "../components/organisms/Dashboard/DashboardHero/DashboardHero";
 
 const Dashboard: React.FC = () => {
 	return (
@@ -22,18 +24,31 @@ const Dashboard: React.FC = () => {
 			<Sidebar />
 			<IonPage id="main-content">
 				<Navbar />
-				<IonContent color="primary">
-					<div>
-						{/* something cool here */}
-					</div>
-					<div className={style.cardsRow3}>
-						{statlist.map((maps, index) => (
-							<StatsCard key={index} label={maps.label} value={maps.value} />
-						))}
-					</div>
-					<div className={style.cardsRow2}>
-						<ExamListCard passed/>
-						<ExamListCard />
+				<IonContent className="ion-padding" color="">
+					<main className={style.col}>
+						<div>
+							<IonCard className="ion-padding" color={"light"}>
+								<main className={style.MainCard}>
+									<img alt="IMAGES" className="" src="/icon/OnEx Hi.svg" />
+								</main>
+							</IonCard>
+						</div>
+						<div>
+							<DashboardHero />
+						</div>
+
+						<div className={style.cardsRow3}>
+							{statlist.map((maps, index) => (
+								<StatsCard key={index} label={maps.label} value={maps.value} />
+							))}
+						</div>
+						<div className={style.cardsRow2}>
+							<ExamListCard passed />
+							<ExamListCard />
+						</div>
+					</main>
+					<div id="contactBox">
+						contactbox
 					</div>
 				</IonContent>
 			</IonPage>
@@ -49,8 +64,8 @@ const statlist = [
 		value: passedLength,
 	},
 	{
-		label: "Time Spent Today",
-		value: "3hr",
+		label: "Class Attended",
+		value: "1",
 	},
 	{
 		label: "Upcoming Exams",
