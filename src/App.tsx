e7107c57b94897c4c7bec1c9d0520dc5bf67e3c8
Tsynblh <hasanuddintn21d@student.unhas.ac.id>
2023-11-25 21:33:50 +0800

    feat: App (update)

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,7 @@ import ExamDetail from "./pages/ExamDetail";
 import Help from "./pages/Help";
 import CreateExamPage from "./pages/CreateExamPage";
 import OnExam from "./pages/OnExam";
+import ExamResult from "./pages/ExamResult";
 import { ProtectedRoute } from "./pages/Auth/ProtectedRoute";
 import Profile from "./pages/Profile";
 setupIonicReact();
@@ -52,6 +53,7 @@ const App: React.FC = () => (
           <ProtectedRoute exact path="/create/exam" component={CreateExamPage} />
 					<ProtectedRoute exact path="/join/:examId" component={ExamDetail}/>
 					<ProtectedRoute exact path="/join/:examId/start" component={OnExam}/>
+					<ProtectedRoute exact path="/join/:examId/result" component={ExamResult}/>
 					<ProtectedRoute exact path="/profile" component={Profile}/>
 					
 					<Route exact path="/">
