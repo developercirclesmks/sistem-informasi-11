539cc56e017252ce445ae9da6cc48de11c03aa8f
sasabilas <salsabilasuryawan2gmail.com>
2023-11-28 21:49:01 +0800

    feat : fix contact us

--- src/components/organisms/ContactUs/ContactUs.tsx
+++ src/components/organisms/ContactUs/ContactUs.tsx
@@ -1,33 +1,39 @@
-import { IonButton, IonImg, IonInput, IonText, IonTextarea } from '@ionic/react'
-import style from './ContactUs.module.css'
+import {
+	IonButton,
+	IonImg,
+	IonInput,
+	IonText,
+	IonTextarea,
+} from "@ionic/react";
+import style from "./ContactUs.module.css";
 
 const ContactUs: React.FC = () => {
-    return (
-        <section className={style.main}>
-            <div className={style.contact}>
-                <IonText className={style.title}>
-                    Questions?
-                </IonText>
-                <IonText className={style.subtitle}>
-                    Contact us!
-                </IonText>
-                <div className={style.form}>
-                    <div className={style.name}>
-                        <IonInput className={style.input} placeholder="First Name"/>
-                        <IonInput className={style.input} placeholder="Last Name"/>
-                    </div>
-                    <IonInput className={style.email} placeholder="Email"/>
-                    <IonTextarea className={style.message} placeholder="Message" />
-                    <IonButton className={style.button} shape="round">
-                        Send
-                    </IonButton>
-                </div>
-            </div>
-            <div className={style.contact}>
-                <IonImg draggable={false} src="./images/ContactUs.png" alt="Contact Us"/>
-            </div>
-        </section>
-    )
-}
+	return (
+		<section className={style.main}>
+			<div className={style.contact}>
+				<IonText className={style.title}>Questions?</IonText>
+				<IonText className={style.subtitle}>Contact us!</IonText>
+				<div className={style.form}>
+					<div className={style.name}>
+						<IonInput fill={"outline"} placeholder="First Name" />
+						<IonInput fill={"outline"} placeholder="Last Name" />
+					</div>
+					<IonInput fill={"outline"} placeholder="Email" />
+					<IonTextarea fill="outline" placeholder="Message" />
+					<IonButton className={style.button} shape="round">
+						Send
+					</IonButton>
+				</div>
+			</div>
+			<div className={style.contact}>
+				<IonImg
+					draggable={false}
+					src="./images/ContactUs.png"
+					alt="Contact Us"
+				/>
+			</div>
+		</section>
+	);
+};
 
-export default ContactUs
+export default ContactUs;
