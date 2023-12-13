8144edd3ab28efd674817d068cb43cc67f863a11
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-12-13 19:32:02 +0800

    fix: notfound

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,63 +1,57 @@
 import {
-  IonButton,
-  IonCol,
-  IonContent,
-  IonGrid,
-  IonHeader,
-  IonPage,
-  IonRow,
-  IonText,
-  IonTitle,
-  IonToolbar,
+	IonButton,
+	IonCol,
+	IonContent,
+	IonGrid,
+	IonHeader,
+	IonPage,
+	IonRow,
+	IonText,
+	IonTitle,
+	IonToolbar,
 } from "@ionic/react";
 import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom"; 
+import { useHistory } from "react-router-dom";
 import Navbar from "../components/organisms/Navbar/Navbar";
 import style from "./styles/NotFound.module.css";
 
 const NotFound: React.FC = () => {
-  const history = useHistory(); 
-  const [redirectTimer, setRedirectTimer] = useState(7); 
+	const history = useHistory();
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      if (redirectTimer > 0) {
-        setRedirectTimer((prevTimer) => prevTimer - 1);
-      } else {
-        clearInterval(timer); 
-        redirectToHome()
-        watisdis();
-      }
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, [redirectTimer]);
-
-  const redirectToHome = () => {
-    history.push("/"); 
-  };
-
-  const watisdis =()=> {
-    console.log(("redirect"));
-  }
-  return (
-    <IonPage>
-      <IonHeader>
-        <Navbar />
-      </IonHeader>
-      <IonContent fullscreen>
-        <main className={style.main}>
-          <div className={style.container}>
-            <img src="./icon/404.svg" alt="logos" draggable={false} />
-            <IonText color='primary' className={style.text}>
-              Oops, it seems you've ventured off the path!
-            </IonText>
-            <IonButton size="large" shape="round"  onClick={redirectToHome}>Back To Home in... {redirectTimer}</IonButton>
-          </div>
-        </main>
-      </IonContent>
-    </IonPage>
-  );
+	return (
+		<IonPage>
+			<IonHeader>
+				<Navbar />
+			</IonHeader>
+			<IonContent fullscreen>
+				<main className={style.main}>
+					<div className={style.container}>
+						<img src="./icon/404.svg" alt="logos" draggable={false} />
+						<IonText color="primary" className={style.text}>
+							Oops, it seems you've ventured off the path!
+						</IonText>
+						<div className={style.buttonGroup}>
+							<IonButton
+								size="default"
+								className={`light ${style.button}`}
+								onClick={() => history.push("/")}
+							>
+								Back To Home
+							</IonButton>
+							<IonButton
+								size="default"
+								className={`light ${style.button}`}
+								fill="outline"
+								onClick={() => history.goBack()}
+							>
+								Back
+							</IonButton>
+						</div>
+					</div>
+				</main>
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default NotFound;
