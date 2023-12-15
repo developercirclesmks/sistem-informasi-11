20f66640e054917feedc995d093b7f47484dd3a2
Tsynblh <hasanuddintn21d@student.unhas.ac.id>
2023-12-15 19:17:22 +0800

    feat : AboutUs Page (update)

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -9,11 +9,16 @@ import {
 import React from "react";
 import AboutUsDesc from "../components/organisms/AboutUsDesc/AboutUsDesc";
 import Navbar from "../components/organisms/Navbar/Navbar";
-import style from "./AboutUs.module.css";
+import style from "./styles/AboutUs.module.css";
 import AboutUsCard from "../components/molecules/aboutuscard/AboutUsCard";
 import AboutUsMission from "../components/organisms/AboutUsMission/AboutUsMission";
 import Footer from "../components/organisms/Footer/Footer";
 import Sidebar from "../components/organisms/Sidebar/Sidebar";
+
+import Dilul from "../../public/images/Dilul.png";
+import Tasya from "../../public/images/Tasya.png";
+import Richal from "../../public/images/Richal.png";
+import Sasa from "../../public/images/Sasa.png";
 
 const AboutUs: React.FC = () => {
   return (
@@ -31,7 +36,7 @@ const AboutUs: React.FC = () => {
           <span className={style.teamTitle}>Meet The Team</span>
           <section className={style.card}>
             {AboutUsList.map((card, index) => (
-              <AboutUsCard key={index} name={card.name} occupations={card.occupations} desc={card.desc}/>
+              <AboutUsCard key={index} img={card.img} name={card.name} occupations={card.occupations} desc={card.desc}/>
             ))}
           </section>
         </main>
@@ -48,23 +53,27 @@ export default AboutUs;
 
 const AboutUsList =[
   {
+    img: Dilul,
     name: "A. Fadhilul Asyam H.",
     occupations: "Informatic Engineering Student",
-    desc: "lalalala"
+    desc: "Criticism is like a brick, use it to build a foundation, not a wall, nor throwing it to people."
   },
   {
+    img: Tasya,
     name: "Tasya Nabila Hasanuddin",
     occupations: "Informatic Engineering Student",
-    desc: "lalalala"
+    desc: "The only way to do great work is to love what you do."
   },
   {
+    img: Richal,
     name: "Richal Akbar",
     occupations: "Informatic Engineering Student",
-    desc: "lalalala"
+    desc: "Education is the most powerful weapon which you can use to change the world."
   },
   {
+    img: Sasa,
     name: "Nur Salzabila Suryawan",
     occupations: "Informatic Engineering Student",
-    desc: "lalalala"
+    desc: "Your time is limited, don't waste it living someone else's life."
   },
 ]
