e6e4601314dac01ecb0d47945fc65fc2ebf8752c
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-12-17 15:04:07 +0800

    fix:responsive about us

--- src/components/molecules/aboutuscard/AboutUsCard.tsx
+++ src/components/molecules/aboutuscard/AboutUsCard.tsx
@@ -1,50 +1,45 @@
 import {
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
+	IonCard,
+	IonCardContent,
+	IonCardHeader,
+	IonCardSubtitle,
+	IonCardTitle,
 } from "@ionic/react";
 import React from "react";
 import style from "./AboutUsCard.module.css";
 
 interface AboutUsCardInterface {
-  name: string;
-  occupations: string;
-  desc: string;
-  img: any;
+	name: string;
+	occupations: string;
+	desc: string;
+	img: any;
 }
 
 const AboutUsCard: React.FC<AboutUsCardInterface> = (props) => {
-  const { name, occupations, desc, img } = props;
-  return (
-    <main className={style.main}>
-      <IonCard className={style.ionCard}>
-        <section className={style.image}>
-          <img
-            className={style.img}
-            alt="img"
-            src={img}
-          />
-        </section>
+	const { name, occupations, desc, img } = props;
+	return (
+		<main className={style.main}>
+			<IonCard className={`noPadding noMargin ${style.ionCard}`}>
+				<section className={style.image}>
+					<img className={style.img} alt="img" src={img} />
+				</section>
+				<section className={style.contentCtn}>
+					<IonCardHeader className={`noPadding noMargin ${style.sectionHeader}`}>
+						<IonCardTitle color={"primary"} className={style.name}>
+							{name}
+						</IonCardTitle>
+						<IonCardSubtitle color={"primary"} className={`noPadding noMargin ${style.subtitle}`}>
+							{occupations}
+						</IonCardSubtitle>
+					</IonCardHeader>
 
-        <section className={style.contentCtn}>
-          <IonCardHeader className={style.sectionHeader}>
-            <IonCardTitle color={"primary"} className={style.name}>
-              {name}
-            </IonCardTitle>
-            <IonCardSubtitle color={"primary"} className={style.subtitle}>
-              {occupations}
-            </IonCardSubtitle>
-          </IonCardHeader>
-
-          <IonCardContent color={"primary"} className={style.sectionContent}>
-            {desc}
-          </IonCardContent>
-        </section>
-      </IonCard>
-    </main>
-  );
+					<IonCardContent color={"primary"} className={`noPadding noMargin ${style.sectionContent}`}>
+						{desc}
+					</IonCardContent>
+				</section>
+			</IonCard>
+		</main>
+	);
 };
 
 export default AboutUsCard;
